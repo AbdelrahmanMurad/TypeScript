a9556fe3d49e49be8ecfbc0056e05170344fbd3c
AbdelrahmanMurad <awmurad2001@gmail.com>
2024-10-22 17:59:22 +0300

    Modules

---
+++ 7.Modules/src/index.ts
@@ -0,0 +1,8 @@
+import { Circle, Square, Rectangle } from './shapes';
+//uncomment the "moduleResolution": "node10" in tsconfig.
+
+let circle = new Circle();
+let square = new Square();
+let rectangle = new Rectangle();
+
+// ... code
---
+++ 7.Modules/src/shapes/index.ts
@@ -0,0 +1,13 @@
+// import { Circle } from './Circle';
+// import { Rectangle } from './Rectangle';
+// import { Square } from './Square';
+
+// // Create instances of each shape + operations
+
+// export { Circle, Rectangle, Square };
+
+// or use Re-exporting Shorthand.
+
+export { Circle } from './Circle';
+export { Rectangle } from './Rectangle';
+export { Square } from './Square';
